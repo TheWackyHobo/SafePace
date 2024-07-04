6cc3965cd414c36261ca4039fbc3cc9d7c37101e
TheWackyHobo <keeganpham78@gmail.com>
2024-07-03 21:26:54 -0500

    working on sensors

--- sensors.py
+++ sensors.py
@@ -1,3 +1,11 @@
-print("poop")
-print("test")
-print("help")
+import smbus2
+
+# Define your I2C settings
+i2c_bus_number = 1  # I2C bus (usually 1 on Raspberry Pi)
+left = 0xE2 
+right = 0xE4
+back = 0xE6
+command_reg = 0x00  
+
+with SMBus(i2c_bus_number) as bus:
+    
