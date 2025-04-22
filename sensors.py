636782257ab27af1cd9b7f4fc1e04f4f71e0a218
TheWackyHobo <keeganpham78@gmail.com>
2025-04-22 16:30:21 -0500

    help

--- sensors.py
+++ sensors.py
@@ -3,7 +3,7 @@ import time
 from motors import motor
 from camera import record_video
 from button_no_edge import was_button_pressed
-
+from sms import send_email
 i2c_bus_number = 1  
 left = 0x71
 right = 0x72
@@ -14,6 +14,10 @@ distance_high_reg = 0x02  # High byte of the distance result
 distance_low_reg = 0x03  # Low byte of the distance result
 check = False
 camera_check = 0
+recipient_email = "keegan.pham@slu.edu" 
+message = "SafePace test"
+sender_email = "safe.pac3@gmail.com"
+sender_password = "vpqo hufr cetl bord"
 def read_distance(bus, sensor_address):
     try:
         # Write the command to initiate ranging in inches
@@ -41,13 +45,14 @@ with smbus2.SMBus(i2c_bus_number) as bus:
                 print(f"Skipping sensor at {hex(sensor)} due to error")
                 continue  # Explicitly move to the next sensorcode
 
-            if 7 < distance < 84:
+            if 7 < distance < 12: #change to 84 for 7ft
                 print(f"Sensor at {hex(sensor)}: {distance:.2f} inches")
                 camera_check = 1
                 motor(distance, sensor)
 
         if(check == True):
             print("ahhhhhh, scary mode")
+            send_email("helppppp","jason.nguyen.1@slu.edu" )
         else:
             print("safe")
         if(camera_check ==1):
